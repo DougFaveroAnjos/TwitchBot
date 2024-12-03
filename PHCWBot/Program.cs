83bc42c26e3c3a104fa0fb755c1741b8338d3cd3
DougFaveroAnjos <dougfavero1@gmail.com>
2024-12-02 22:24:26 -0300

    Realizado implementação do bot no chat do streamer

---
+++ PHCWBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using TwitchTools;
+
+namespace PHCWBot
+{
+    class Program
+    {
+        static string oauth = "oauth:(oauth Token)";
+        static TwitchChatHandle chat = new TwitchChatHandle(oauth, "PHCWB");
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var lerMensagem = chat.Read();
+                string mensagem = lerMensagem.Message;
+
+                if(mensagem.Contains("!phcwbot") || mensagem.Contains("!PHCWBot") || mensagem.Contains("!PHCWBOT"))
+                {
+                    chat.Write("mensagem");
+                }
+            }
+        }
+    }
+}
